fcce6746d09f5f9e3db87d0f175da654970e4778
Luciano E. Montiero <luciano.es.mo@gmail.com>
2021-12-10 16:18:53 -0300

    Ex 6 - extra 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,49 +1,178 @@
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import * as React from 'react';
+import {
+  PokemonDataView,
+  PokemonForm,
+  PokemonInfoFallback,
+  fetchPokemon,
+} from '../pokemon';
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+// Ex 6
 
-  // 💣 remove this
-  return 'TODO'
+// function PokemonInfo({pokemonName}) {
+//   const [pokemon, setPokemon] = React.useState(null)
+
+//   React.useEffect(() => {
+//     if (!pokemonName) {
+//       return
+//     }
+//     setPokemon(null)
+//     fetchPokemon(pokemonName).then(pokemon => setPokemon(pokemon))
+//   }, [pokemonName])
+
+//   if (!pokemonName) {
+//     return 'Submit a pokemon'
+//   } else if (!pokemon) {
+//     return <PokemonInfoFallback name={pokemonName} />
+//   } else {
+//     return <PokemonDataView pokemon={pokemon} />
+//   }
+// }
+
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('');
+//   const [error, setError] = React.useState(null);
+
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName);
+//   }
+
+//   return (
+//     <div className='pokemon-info-app'>
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className='pokemon-info'>
+//         <PokemonInfo
+//           pokemonName={pokemonName}
+//           errorState={{ error, setError }}
+//         />
+//       </div>
+//       {error === null ? (
+//         ''
+//       ) : (
+//         <div role='alert'>
+//           There was an error:{' '}
+//           <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// Ex 6 - extra 1
+
+// function PokemonInfo({ pokemonName }) {
+//   const [pokemon, setPokemon] = React.useState(null);
+//   const [error, setError] = React.useState(null);
+
+//   React.useEffect(() => {
+//     if (!pokemonName) {
+//       return;
+//     }
+//     setPokemon(null);
+//     setError(null);
+//     fetchPokemon(pokemonName)
+//       .then(pokomonData => setPokemon(pokomonData))
+//       .catch(error => setError(error));
+//   }, [pokemonName, setError]);
+
+//   if (error) {
+//     return (
+//       <div role='alert'>
+//         There was an error:{' '}
+//         <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>{' '}
+//       </div>
+//     );
+//   }
+
+//   if (!pokemonName) {
+//     return 'Submit a pokemon';
+//   } else if (!pokemon) {
+//     return <PokemonInfoFallback name={pokemonName} />;
+//   } else {
+//     return <PokemonDataView pokemon={pokemon} />;
+//   }
+// }
+
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('');
+
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName);
+//   }
+
+//   return (
+//     <div className='pokemon-info-app'>
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className='pokemon-info'>
+//         <PokemonInfo pokemonName={pokemonName} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// Ex 6 - extra 2
+
+function PokemonInfo({ pokemonName }) {
+  const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [status, setStatus] = React.useState('idle');
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    setPokemon(null);
+    setError(null);
+    setStatus('pending');
+    fetchPokemon(pokemonName)
+      .then(pokomonData => {
+        setPokemon(pokomonData);
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
+  }, [pokemonName, setError, setStatus]);
+
+  if (status === 'rejected') {
+    return (
+      <div role='alert'>
+        There was an error:{' '}
+        <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>{' '}
+      </div>
+    );
+  }
+
+  if (status === 'idle') {
+    return 'Submit a pokemon';
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />;
+  } else {
+    return <PokemonDataView pokemon={pokemon} />;
+  }
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
+  const [pokemonName, setPokemonName] = React.useState('');
 
   function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
+    setPokemonName(newPokemonName);
   }
 
   return (
-    <div className="pokemon-info-app">
+    <div className='pokemon-info-app'>
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
-      <div className="pokemon-info">
+      <div className='pokemon-info'>
         <PokemonInfo pokemonName={pokemonName} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
