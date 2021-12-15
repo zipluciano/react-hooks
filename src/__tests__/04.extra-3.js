bb957a5f0be98020baab20e35bac4a2585ab93c7
Luciano E. Montiero <luciano.es.mo@gmail.com>
2021-12-15 12:50:58 -0300

    Ex 4  - extra 3

--- src/__tests__/04.extra-3.js
+++ src/__tests__/04.extra-3.js
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import App from '../final/04.extra-3'
-// import App from '../exercise/04'
+import * as React from 'react';
+import { alfredTip } from '@kentcdodds/react-workshop-app/test-utils';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import App from '../final/04.extra-3'
+import App from '../exercise/04';
 
 test('can play a game of tic tac toe', () => {
-  render(<App />)
+  render(<App />);
 
   // prettier-ignore
   const [
@@ -14,39 +14,39 @@ test('can play a game of tic tac toe', () => {
     s4, s5, s6, // eslint-disable-line no-unused-vars
     s7, s8, s9 // eslint-disable-line no-unused-vars
   ] = Array.from(screen.queryAllByRole('button'))
-  expect(screen.getByText('Next player: X')).toBeInTheDocument()
-  const gameStart = screen.getByText(/go to game start/i)
-  expect(gameStart).toHaveAttribute('disabled')
-  expect(gameStart).toHaveTextContent('current')
+  expect(screen.getByText('Next player: X')).toBeInTheDocument();
+  const gameStart = screen.getByText(/go to game start/i);
+  expect(gameStart).toHaveAttribute('disabled');
+  expect(gameStart).toHaveTextContent('current');
 
-  userEvent.click(s1)
-  expect(s1).toHaveTextContent('X')
+  userEvent.click(s1);
+  expect(s1).toHaveTextContent('X');
 
-  expect(screen.getByText('Next player: O')).toBeInTheDocument()
-  const firstMove = screen.getByText(/go to move #1/i)
-  expect(gameStart).not.toHaveAttribute('disabled')
-  expect(gameStart).not.toHaveTextContent('current')
-  expect(firstMove).toHaveAttribute('disabled')
-  expect(firstMove).toHaveTextContent('current')
+  expect(screen.getByText('Next player: O')).toBeInTheDocument();
+  const firstMove = screen.getByText(/go to move #1/i);
+  expect(gameStart).not.toHaveAttribute('disabled');
+  expect(gameStart).not.toHaveTextContent('current');
+  expect(firstMove).toHaveAttribute('disabled');
+  expect(firstMove).toHaveTextContent('current');
 
-  userEvent.click(s5)
-  expect(s5).toHaveTextContent('O')
-  const secondMove = screen.getByText(/go to move #2/i)
-  expect(gameStart).not.toHaveAttribute('disabled')
-  expect(gameStart).not.toHaveTextContent('current')
-  expect(firstMove).not.toHaveAttribute('disabled')
-  expect(firstMove).not.toHaveTextContent('current')
-  expect(secondMove).toHaveAttribute('disabled')
-  expect(secondMove).toHaveTextContent('current')
+  userEvent.click(s5);
+  expect(s5).toHaveTextContent('O');
+  const secondMove = screen.getByText(/go to move #2/i);
+  expect(gameStart).not.toHaveAttribute('disabled');
+  expect(gameStart).not.toHaveTextContent('current');
+  expect(firstMove).not.toHaveAttribute('disabled');
+  expect(firstMove).not.toHaveTextContent('current');
+  expect(secondMove).toHaveAttribute('disabled');
+  expect(secondMove).toHaveTextContent('current');
 
-  userEvent.click(firstMove)
-  expect(gameStart).not.toHaveAttribute('disabled')
-  expect(gameStart).not.toHaveTextContent('current')
-  expect(firstMove).toHaveAttribute('disabled')
-  expect(firstMove).toHaveTextContent('current')
-  expect(secondMove).not.toHaveAttribute('disabled')
-  expect(secondMove).not.toHaveTextContent('current')
-  expect(s5).not.toHaveTextContent('O')
+  userEvent.click(firstMove);
+  expect(gameStart).not.toHaveAttribute('disabled');
+  expect(gameStart).not.toHaveTextContent('current');
+  expect(firstMove).toHaveAttribute('disabled');
+  expect(firstMove).toHaveTextContent('current');
+  expect(secondMove).not.toHaveAttribute('disabled');
+  expect(secondMove).not.toHaveTextContent('current');
+  expect(s5).not.toHaveTextContent('O');
 
   alfredTip(
     () =>
@@ -67,17 +67,17 @@ test('can play a game of tic tac toe', () => {
         ],
       ),
     'Make sure that the localStorage item is updated with the JSON.stringified squares array',
-  )
+  );
 
-  userEvent.click(gameStart)
-  expect(s1).toHaveTextContent('')
-  expect(s5).toHaveTextContent('')
-  expect(screen.queryAllByRole('listitem').length).toBe(3)
+  userEvent.click(gameStart);
+  expect(s1).toHaveTextContent('');
+  expect(s5).toHaveTextContent('');
+  expect(screen.queryAllByRole('listitem').length).toBe(3);
 
-  userEvent.click(screen.getByText('restart'))
-  expect(s1).toHaveTextContent('')
-  expect(s5).toHaveTextContent('')
-  expect(screen.queryAllByRole('listitem').length).toBe(1)
+  userEvent.click(screen.getByText('restart'));
+  expect(s1).toHaveTextContent('');
+  expect(s5).toHaveTextContent('');
+  expect(screen.queryAllByRole('listitem').length).toBe(1);
 
   alfredTip(
     () =>
@@ -92,5 +92,5 @@ test('can play a game of tic tac toe', () => {
         ],
       ),
     'Make sure that the localStorage item is updated with the JSON.stringified squares array',
-  )
-})
+  );
+});
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,46 +2,362 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react';
+import { useLocalStorageState } from '../utils';
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null);
-  console.log(squares);
+// Ex 4
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
+// function Board() {
+//   const [squares, setSquares] = React.useState(Array(9).fill(null));
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
+//   // Variables to evaluate the game status
+//   const nextValue = calculateNextValue(squares);
+//   const winner = calculateWinner(squares);
+//   const status = calculateStatus(winner, squares, nextValue);
 
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
+//   function selectSquare(square) {
+//     if (winner || squares[square]) {
+//       // Don't make any changes
+//       return;
+//     }
 
+//     const helperArray = [...squares];
+//     helperArray[square] = nextValue;
+//     setSquares(helperArray);
+//   }
+
+//   function restart() {
+//     setSquares(Array(9).fill(null));
+//   }
+
+//   function renderSquare(i) {
+//     return (
+//       <button className='square' onClick={() => selectSquare(i)}>
+//         {squares[i]}
+//       </button>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <div className='status'>{status}</div>
+//       <div className='board-row'>
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//       <button className='restart' onClick={restart}>
+//         restart
+//       </button>
+//     </div>
+//   );
+// }
+
+// function Game() {
+//   return (
+//     <div className='game'>
+//       <div className='game-board'>
+//         <Board />
+//       </div>
+//     </div>
+//   );
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`;
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
+
+// function App() {
+//   return <Game />;
+// }
+
+// Ex 4 - extra 1
+
+// function Board() {
+//   function getDataLocalStorage() {
+//     return localStorage.getItem('squares') === null || undefined
+//       ? Array(9).fill(null)
+//       : JSON.parse(localStorage.getItem('squares'));
+//   }
+
+//   function setDataLocalStorage(array) {
+//     const squaresArray = JSON.stringify(array);
+//     localStorage.setItem('squares', squaresArray);
+//   }
+
+//   const [squares, setSquares] = React.useState(() => getDataLocalStorage());
+
+//   React.useEffect(() => {
+//     setDataLocalStorage(squares);
+//   }, [squares]);
+
+//   // Variables to evaluate the game status
+//   const nextValue = calculateNextValue(squares);
+//   const winner = calculateWinner(squares);
+//   const status = calculateStatus(winner, squares, nextValue);
+
+//   function selectSquare(square) {
+//     if (winner || squares[square]) {
+//       // Don't make any changes
+//       return;
+//     }
+
+//     const helperArray = [...squares];
+//     helperArray[square] = nextValue;
+//     setSquares(helperArray);
+//   }
+
+//   function restart() {
+//     setSquares(Array(9).fill(null));
+//   }
+
+//   function renderSquare(i) {
+//     return (
+//       <button className='square' onClick={() => selectSquare(i)}>
+//         {squares[i]}
+//       </button>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <div className='status'>{status}</div>
+//       <div className='board-row'>
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//       <button className='restart' onClick={restart}>
+//         restart
+//       </button>
+//     </div>
+//   );
+// }
+
+// function Game() {
+//   return (
+//     <div className='game'>
+//       <div className='game-board'>
+//         <Board />
+//       </div>
+//     </div>
+//   );
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`;
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
+
+// function App() {
+//   return <Game />;
+// }
+
+// Ex 4 - extra 2
+
+// function Board() {
+//   const [squares, setSquares] = useLocalStorageState(
+//     'squares',
+//     Array(9).fill(null),
+//   );
+
+//   function setDataLocalStorage(array) {
+//     const squaresArray = JSON.stringify(array);
+//     localStorage.setItem('squares', squaresArray);
+//   }
+
+//   React.useEffect(() => {
+//     setDataLocalStorage(squares);
+//   }, [squares]);
+
+//   // Variables to evaluate the game status
+//   const nextValue = calculateNextValue(squares);
+//   const winner = calculateWinner(squares);
+//   const status = calculateStatus(winner, squares, nextValue);
+
+//   function selectSquare(square) {
+//     if (winner || squares[square]) {
+//       // Don't make any changes
+//       return;
+//     }
+
+//     const helperArray = [...squares];
+//     helperArray[square] = nextValue;
+//     setSquares(helperArray);
+//   }
+
+//   function restart() {
+//     setSquares(Array(9).fill(null));
+//   }
+
+//   function renderSquare(i) {
+//     return (
+//       <button className='square' onClick={() => selectSquare(i)}>
+//         {squares[i]}
+//       </button>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <div className='status'>{status}</div>
+//       <div className='board-row'>
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className='board-row'>
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//       <button className='restart' onClick={restart}>
+//         restart
+//       </button>
+//     </div>
+//   );
+// }
+
+// function Game() {
+//   return (
+//     <div className='game'>
+//       <div className='game-board'>
+//         <Board />
+//       </div>
+//     </div>
+//   );
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`;
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
+
+// function App() {
+//   return <Game />;
+// }
+
+// Ex 4 - extra 3
+
+function Board({ squares, onClick }) {
   function renderSquare(i) {
     return (
-      <button className='square' onClick={() => selectSquare(i)}>
+      <button className='square' onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     );
@@ -49,8 +365,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className='status'>STATUS</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
@@ -66,24 +380,70 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className='restart' onClick={restart}>
-        restart
-      </button>
     </div>
   );
 }
 
 function Game() {
+  const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [
+    Array(9).fill(null),
+  ]);
+  const [currentStep, setCurrentStep] = useLocalStorageState(
+    'tic-tac-toe:step',
+    0,
+  );
+
+  const currentSquares = history[currentStep];
+  const winner = calculateWinner(currentSquares);
+  const nextValue = calculateNextValue(currentSquares);
+  const status = calculateStatus(winner, currentSquares, nextValue);
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) {
+      return;
+    }
+
+    const newHistory = history.slice(0, currentStep + 1);
+    const squares = [...currentSquares];
+
+    squares[square] = nextValue;
+    setHistory([...newHistory, squares]);
+    setCurrentStep(newHistory.length);
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentStep(0);
+  }
+
+  const moves = history.map((stepSquares, step) => {
+    const desc = step ? `Go to move #${step}` : 'Go to game start';
+    const isCurrentStep = step === currentStep;
+    return (
+      <li key={step}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+          {desc} {isCurrentStep ? '(current)' : null}
+        </button>
+      </li>
+    );
+  });
+
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className='restart' onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className='game-info'>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
@@ -92,12 +452,10 @@ function calculateStatus(winner, squares, nextValue) {
     : `Next player: ${nextValue}`;
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
